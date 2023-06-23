069d7685ff022f49ac724d857ab523672f4a07ce
Nhat Pham <nhat2up@gmail.com>
2023-06-23 08:32:24 +0700

    Cart templete

--- BirdMeal/BirdMeal/Pages/Cart.cshtml.cs
+++ BirdMeal/BirdMeal/Pages/Cart.cshtml.cs
@@ -1,12 +1,40 @@
+using BusinessObjects.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Repository.UserRepository;
+using System.Windows;
 
 namespace BirdMeal.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
-        {
-        }
-    }
+		private IUserRepository userRepository { get; set; }
+
+		public CartModel()
+		{
+			userRepository = new UserRepository();	
+		}
+		public IActionResult OnGet()
+		{
+			string loginMem = HttpContext.Session.GetString("loginMem");
+			if (loginMem != null)
+			{
+				User u = userRepository.GetUserByEmail(loginMem);
+				if (u != null && u.Role.Equals("CUSTOMER"))
+				{
+					return Page();
+				}
+			}
+			else
+			{
+				return RedirectToPage("/Login");
+			}
+			return RedirectToPage("/Login");
+		}
+
+		public void OnPost(float? total)
+		{
+			MessageBox.Show(total.ToString());
+		}
+	}
 }
